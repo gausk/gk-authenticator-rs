8ff6943f30f305ba0136234660e8de4fbebd0f58
Gaurav Kumar <gauravsingh12341@gmail.com>
2025-10-20 01:45:38 +0530

    Basic cli defined

---
+++ src/command/mod.rs
@@ -0,0 +1,70 @@
+use clap::{Parser, Subcommand, ValueEnum};
+use data_encoding::BASE32_NOPAD;
+
+#[derive(Parser, Debug)]
+pub struct Arg {
+    #[clap(subcommand)]
+    pub command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Command {
+    #[clap(about = "Add an account")]
+    Add {
+        #[arg(long, help = "Name of the account")]
+        account: String,
+        #[arg(
+            short,
+            long,
+            help = "Secret key of the OTP",
+            value_parser(is_base32_key)
+        )]
+        key: String,
+        #[arg(long, help = "Time based account", conflicts_with = "htop")]
+        totp: bool,
+        #[arg(long, help = "Counter based account", conflicts_with = "totp")]
+        htop: bool,
+        #[arg(
+            short,
+            long,
+            help = "Algorithm to use to generate the OTP code",
+            default_value_t = Algorithm::Sha1,
+            value_enum
+        )]
+        algorithm: Algorithm,
+    },
+    #[clap(about = "Delete an account")]
+    Delete {
+        #[clap(long, help = "Name of the account")]
+        account: String,
+    },
+    #[clap(about = "List OTP for all accounts")]
+    List {
+        #[arg(short, long, help = "Length of the OTP", default_value_t = 6)]
+        length: u64,
+    },
+    #[clap(about = "List OTP for a particular account")]
+    View {
+        #[arg(long, help = "Name of the account")]
+        account: String,
+        #[arg(short, long, help = "Length of the OTP", default_value_t = 6)]
+        length: u64,
+    }
+}
+
+#[derive(Copy, Clone, Debug, ValueEnum)]
+pub enum Algorithm {
+    Sha1,
+    Sha256,
+    Sha384,
+    Sha512,
+}
+
+// Validate key provided has base32 encoding
+fn is_base32_key(key: &str) -> Result<String, String> {
+    let key = key.to_uppercase();
+    match BASE32_NOPAD.decode(key.as_bytes()) {
+        Ok(_) => Ok(key),
+        Err(e) => Err(format!("Base32 decode error: {}", e)),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+#![allow(unused)]
+mod command;
+
+use clap::Parser;
+use command::Arg;
+use crate::command::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let arg = Arg::parse();
+    match arg.command {
+        Command::Add { account, key, htop, totp, algorithm } => {
+
+        }
+        Command::Delete { account} => {
+
+        }
+        Command::List { length} => {
+
+        }
+        Command::View { account, length } => {
+
+        }
+    }
 }
